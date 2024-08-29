50ef753aac5a8d2c3ba55f19a6b76493c5bf0d5c
shokhmadaminov <shokhmadaminov@gmail.com>
2024-08-29 18:03:50 +0500

    add commet section

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const moonIcon = document.querySelector(".header__moon-icon")
 const headerLogin = document.querySelector(".header__login")
 const overlayEl = document.querySelector(".overlay")
 const modalInput = document.querySelector(".modal__input--password")
+const commentList = document.querySelector(".comment__list")
+const commentCarouselEl = document.getElementById("comment-carousel")
+const commentCarouselScroll = document.querySelector(".comment__right-carousel-scroll")
+const commentItem = document.querySelectorAll(".comment__item")
+const commentCarouselBtns = document.querySelector(".comment__right-carousel-btns").children
 
 document.getElementById("dark-btn").addEventListener("click", ()=> {
     moonIcon.classList.toggle("hidden")
@@ -31,3 +36,27 @@ document.querySelector(".modal__eye-btn").addEventListener("click", function() {
         modalInput.setAttribute("type", "password")
     }
 })
+
+
+commentCarouselEl.style.width = `${100 / commentList.children.length}%`
+
+let commentCount = 0
+commentCarouselBtns[0].addEventListener("click", (e)=> {
+    if(commentCount > 0) {
+        commentCount--
+        commentItem.forEach(item=> {
+            item.style.transform = `translateX(-${commentCount}00%)`
+        })
+        commentCarouselEl.style.transform = `translateX(${commentCount}00%)`
+    }
+})
+commentCarouselBtns[1].addEventListener("click", (e)=> {
+    if(commentList.children.length - 1 !== commentCount) {
+        commentCount++ 
+        commentItem.forEach(item=> {
+            item.style.transform = `translateX(-${commentCount}00%)`
+        })
+        commentCarouselEl.style.transform = `translateX(${commentCount}00%)`
+    }
+})
+
